fa7449e60f7286bf99dafcd1359c9ea260120f68
henryur <Henry@DESKTOP-MKT8B2N.attlocal.net>
2021-01-12 19:05:55 -0600

    llamachow drectrory fix

--- src/main/java/net/mcreator/llamamod/item/CookedllamachowItem.java
+++ src/main/java/net/mcreator/llamamod/item/CookedllamachowItem.java
@@ -6,10 +6,10 @@ import net.minecraftforge.registries.ObjectHolder;
 import net.minecraft.item.UseAction;
 import net.minecraft.item.Rarity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Item;
 import net.minecraft.item.Food;
 
+import net.mcreator.llamamod.itemgroup.LlamacreativetabItemGroup;
 import net.mcreator.llamamod.LlamaModModElements;
 
 @LlamaModModElements.ModElement.Tag
@@ -26,7 +26,7 @@ public class CookedllamachowItem extends LlamaModModElements.ModElement {
 	}
 	public static class FoodItemCustom extends Item {
 		public FoodItemCustom() {
-			super(new Item.Properties().group(ItemGroup.FOOD).maxStackSize(64).rarity(Rarity.COMMON)
+			super(new Item.Properties().group(LlamacreativetabItemGroup.tab).maxStackSize(64).rarity(Rarity.COMMON)
 					.food((new Food.Builder()).hunger(40).saturation(10f).build()));
 			setRegistryName("cookedllamachow");
 		}
