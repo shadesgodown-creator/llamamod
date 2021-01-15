fc6724aedbc0ad950790da2f09e5c65b85140590
henryur <Henry@DESKTOP-MKT8B2N.attlocal.net>
2021-01-15 15:42:42 -0600

    new struckers

---
+++ src/main/java/net/mcreator/llamamod/block/SuperlightBlock.java
@@ -0,0 +1,57 @@
+
+package net.mcreator.llamamod.block;
+
+import net.minecraftforge.registries.ObjectHolder;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.storage.loot.LootContext;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.BlockItem;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Block;
+
+import net.mcreator.llamamod.itemgroup.LlamacreativetabItemGroup;
+import net.mcreator.llamamod.LlamaModModElements;
+
+import java.util.List;
+import java.util.Collections;
+
+@LlamaModModElements.ModElement.Tag
+public class SuperlightBlock extends LlamaModModElements.ModElement {
+	@ObjectHolder("llama_mod:superlight")
+	public static final Block block = null;
+	public SuperlightBlock(LlamaModModElements instance) {
+		super(instance, 88);
+	}
+
+	@Override
+	public void initElements() {
+		elements.blocks.add(() -> new CustomBlock());
+		elements.items
+				.add(() -> new BlockItem(block, new Item.Properties().group(LlamacreativetabItemGroup.tab)).setRegistryName(block.getRegistryName()));
+	}
+	public static class CustomBlock extends Block {
+		public CustomBlock() {
+			super(Block.Properties.create(Material.IRON).sound(SoundType.METAL).hardnessAndResistance(1f, 10f).lightValue(15).slipperiness(0.7f));
+			setRegistryName("superlight");
+		}
+
+		@OnlyIn(Dist.CLIENT)
+		@Override
+		public boolean isEmissiveRendering(BlockState blockState) {
+			return true;
+		}
+
+		@Override
+		public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+			List<ItemStack> dropsOriginal = super.getDrops(state, builder);
+			if (!dropsOriginal.isEmpty())
+				return dropsOriginal;
+			return Collections.singletonList(new ItemStack(this, 0));
+		}
+	}
+}
